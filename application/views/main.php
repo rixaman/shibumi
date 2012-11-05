db35517b77bd1924e2a8a470d824cc7cb0f6f217
rixaman <rixaman@gmail.com>
2012-11-05 06:38:56 +0600

    caruosel finish view

--- application/views/main.php
+++ application/views/main.php
@@ -1,7 +1,7 @@
 <div id="myCarousel" class="carousel">
     <!-- Картинки в карусельке -->
     <div class="carousel-inner">
-	    <div class="item"><img src="/public/img/logo.jpg" width="1000"></div>
+	    <div class="item active"><img src="/public/img/logo.jpg" width="1000"></div>
 	    <div class="item"><img src="/public/img/stuff/a1.jpg" width="1000"></div>
 	    <div class="item"><img src="/public/img/stuff/a2.jpg" width="1000"></div>
     </div>

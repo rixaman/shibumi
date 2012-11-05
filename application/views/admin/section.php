8b4c3e3ad97244689dab0da590c1af7d75e374fb
rixaman <rixaman@gmail.com>
2012-11-05 08:53:55 +0600

    rixybox

--- application/views/admin/section.php
+++ application/views/admin/section.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<? foreach($sections as $section) : ?>
 		<div class="span3">
-			<div class="well focus-shadow">
+			<div class="well focus-shadow rixybox">
 				<h2><?= $section->title; ?></h2>
 				<img src="<?=$section->image?>" class="img-circle" width="240" height="202"><br><br>
 				<p><?= $section->description; ?></p>

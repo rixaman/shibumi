c0eaa6627c883b32a0141a7b39c042291fa912a3
rixaman <rixaman@gmail.com>
2012-11-06 19:31:03 +0600

    vk widjet in footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -3,7 +3,7 @@
 		<div class="span4">
 			<div class="alert vk focus-shadow">
 				<div id="vk_groups"></div>
-				<script type="text/javascript" src="//vk.com/js/api/openapi.js?63"></script>
+				<script type="text/javascript" src="//vk.com/js/api/openapi.js"></script>
 				<!-- VK Widget -->
 				<script type="text/javascript">
 				VK.Widgets.Group("vk_groups", {mode: 0, width: "298", height: "200"}, 42669816);
